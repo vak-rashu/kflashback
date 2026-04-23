3c1d602b411ab4ada082703611a6529d2f8971dc
vak-rashu <rashu.cloud11@gmail.com>
2026-04-23 17:56:28 +0000

    Created E2E workflow for SQLite

--- test/postgres_setup.go
+++ test/postgres_setup.go
@@ -15,7 +15,7 @@ var (
 
 func setupPostgres(ctx context.Context, cfg *envconf.Config) (context.Context, error) {
 	log.Println("Setup Sqlite Policy RBAC")
-	if p := utils.RunCommand(fmt.Sprintf("kubectl create secret generic kflashback-db-credentials --namespace=kflashback-system --from-literal=dsn='postgres://kflashback:YOUR_PASSWORD@your-db-host:5432/kflashback?sslmode=require' kubectl apply -f %s",
+	if p := utils.RunCommand(fmt.Sprintf("kubectl create secret generic kflashback-db-credentials --namespace=kflashback-system --from-literal=dsn='postgres://postgres :postgres@localhost:5432/kflashback?sslmode=require' kubectl apply -f %s",
 		postgresPath)); p.Err() != nil {
 		log.Printf("Failed to deploy Postgres Policy: %s: %s", p.Err(), p.Out())
 		return ctx, p.Err()
